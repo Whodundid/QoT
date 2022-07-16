782569a22fcb6fe5f4ec60e5b5631ea12662bf9e
Hunter Troy Bragg <Whodundid@gmail.com>
2022-07-16 12:15:59 -0600

    Readded bush texture

--- src/game/worldTiles/tileCategories/nature/Bush0.java
+++ src/game/worldTiles/tileCategories/nature/Bush0.java
@@ -1,6 +1,5 @@
 package game.worldTiles.tileCategories.nature;
 
-import assets.textures.DoodadTextures;
 import assets.textures.WorldTextures;
 import game.worldTiles.TileIDs;
 import game.worldTiles.WorldTile;
@@ -9,7 +8,7 @@ public class Bush0 extends WorldTile {
 	
 	public Bush0() {
 		super(TileIDs.BUSH0);
-		setTexture(DoodadTextures.pine_tree);
+		setTexture(WorldTextures.bush0);
 	}
 	
 }
