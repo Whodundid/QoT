0824b71ba784af90db243665e8c3d463adf54658
Hunter Troy Bragg <Whodundid@gmail.com>
2022-07-25 00:13:48 -0600

    QOT-11: Fixing menu object draw on new map screen in map editor

--- src/world/mapEditor/NewMapCreatorScreen.java
+++ src/world/mapEditor/NewMapCreatorScreen.java
@@ -45,8 +45,6 @@ public class NewMapCreatorScreen extends GameScreen {
 		}
 	}
 	
-	@Override public void initScreen() {}
-	
 	@Override
 	public void initObjects() {
 		double x = midX - 150;
@@ -55,17 +53,17 @@ public class NewMapCreatorScreen extends GameScreen {
 		back = new WindowButton(this, 5, endY - 45, 150, 40, "Back");
 		
 		nameLabel = new WindowLabel(this, x, y, "Map name");
-		nameField = new WindowTextField(this, nameLabel.startX, nameLabel.endY + 20, 300, 35);
+		nameField = new WindowTextField(this, nameLabel.startX, nameLabel.endY + 5, 300, 35);
 		
-		widthHeightLabel = new WindowLabel(this, x, nameField.endY + 20, "Width / Height");
-		widthField = new WindowTextField(this, x, widthHeightLabel.endY + 25, 100, 35);
-		heightField = new WindowTextField(this, widthField.endX + 10, widthHeightLabel.endY + 25, 100, 35);
+		widthHeightLabel = new WindowLabel(this, x, nameField.endY + 30, "Width / Height");
+		widthField = new WindowTextField(this, x, widthHeightLabel.endY + 5, 100, 35);
+		heightField = new WindowTextField(this, widthField.endX + 10, widthHeightLabel.endY + 5, 100, 35);
 		
-		emptyLabel = new WindowLabel(this, x, widthField.endY + 20, "Empty map");
-		emptyMap = new WindowCheckBox(this, emptyLabel.endX + 20, widthField.endY + 20, 50, 50);
+		emptyLabel = new WindowLabel(this, x, widthField.endY + 42, "Empty map");
+		emptyMap = new WindowCheckBox(this, emptyLabel.endX + 20, widthField.endY + 30, 50, 50);
 		
-		fillWithLabel = new WindowLabel(this, x, emptyMap.endY + 20, "Fill with..");
-		tileSelection = new WindowButton(this, x, fillWithLabel.endY + 20, 45, 45);
+		fillWithLabel = new WindowLabel(this, x, emptyMap.endY + 35, "Fill with");
+		tileSelection = new WindowButton(this, emptyMap.startX, emptyMap.endY + 20, 50, 50);
 		
 		create = new WindowButton(this, midX - 100, tileSelection.endY + 50, 200, 45, "Create");
 		
@@ -75,16 +73,18 @@ public class NewMapCreatorScreen extends GameScreen {
 		fillWithLabel.setVisible(!empty);
 		tileSelection.setVisible(!empty);
 		
-		nameField.setText(name);
+		nameField.setTextWhenEmpty("name");
+		//if (name == null) nameField.setText("name");
+		//else nameField.setText(name);
 		widthField.setText(width + "");
 		heightField.setText(height + "");
 		
-		addObject();
-		addObject(nameField);
-		addObject(widthField, heightField);
-		addObject(emptyMap);
-		addObject(tileSelection);
-		addObject();
+		addObject(back);
+		addObject(nameLabel, nameField);
+		addObject(widthHeightLabel, widthField, heightField);
+		addObject(emptyLabel, emptyMap);
+		addObject(fillWithLabel, tileSelection);
+		addObject(create);
 	}
 
 	@Override
@@ -93,14 +93,12 @@ public class NewMapCreatorScreen extends GameScreen {
 		
 		drawStringC("Create New Map", midX, startY + 50);
 		
-		if (drawError) { drawStringC(error, midX, endY - 150); }
+		if (drawError) drawStringC(error, midX, endY - 150, EColors.lred);
 	}
-	
-	@Override public void onScreenClosed() {}
 	
 	@Override
 	public void actionPerformed(IActionObject object, Object... args) {
-		if (object == back) { closeScreen(); }
+		if (object == back) closeScreen();
 		
 		if (object == emptyMap) {
 			empty = emptyMap.isChecked();
@@ -109,8 +107,13 @@ public class NewMapCreatorScreen extends GameScreen {
 		}
 		
 		if (object == create || object == nameField) {
-			if (check()) { createMap(); }
-			else { drawError = true; }
+			if (check()) createMap();
+			else drawError = true;
+		}
+		
+		if (object == tileSelection) {
+			error = "Not yet implemented!";
+			drawError = true;
 		}
 	}
 	
@@ -128,15 +131,22 @@ public class NewMapCreatorScreen extends GameScreen {
 			error = e.toString();
 		}
 		
-		if (name == null || name.isBlank() || name.isEmpty()) { good = false; error = "Map name cannot be empty!"; }
-		if (width < 0 || height < 0) { good = false; error = "Map width or height cannot be negative!"; }
+		if (name == null || name.isBlank() || name.isEmpty()) {
+			good = false;
+			error = "Map name cannot be empty!";
+		}
+		
+		if (width < 0 || height < 0) {
+			good = false;
+			error = "Map width or height cannot be negative!";
+		}
 		
 		return good;
 	}
 	
 	private void createMap() {
 		world = new GameWorld(name, width, height);
-		if (!empty) { world.fillWith(fillTile); }
+		if (!empty) world.fillWith(fillTile);
 		QoT.displayScreen(new MapEditorScreen(world), this);
 	}
 	
