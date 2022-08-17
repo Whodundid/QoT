37efa35172d1dd7f9106814de68bd68a496b1b40
Hunter Troy Bragg <Whodundid@gmail.com>
2022-08-16 23:28:58 -0600

    QOT-64: Removed temp code to stop drawing walls (might add as debug)

--- src/world/worldTiles/WorldTile.java
+++ src/world/worldTiles/WorldTile.java
@@ -157,12 +157,6 @@ public abstract class WorldTile extends GLObject implements Comparable<WorldTile
 	 */
 	@SuppressWarnings("unused")
 	public void renderTile(GameWorld world, double x, double y, double w, double h, int brightness, boolean mouseOver) {
-//		if (true) {
-//			if (tex != null) drawTexture(tex, x, y, w, h, brightness);
-//			
-//			return;
-//		}
-		
 		double wh = h * wallHeight; //wh == 'wallHeight'
 		
 		WorldTile tb = null; // tb == 'tileBelow'
